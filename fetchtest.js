0abe35481acc03f53c671e2a763398da4dc5f4f6
rodericklgp <roderick_lgp@qq.com>
2024-01-30 11:16:11 +0800

    Update fetchtest.js

--- fetchtest.js
+++ fetchtest.js
@@ -1,5 +1,4 @@
-<script>
- var  test= await fetch('https://example.com/')
- var testtext= await test.text()
- document.write(testtext)
-  </script>
+var  test= await fetch('https://example.com/')
+var testtext= await test.text()
+document.write(testtext)
+
