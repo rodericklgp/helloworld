a54fdcee78047ff76625013f2af39fbbc1947a13
rodericklgp <roderick_lgp@qq.com>
2024-01-30 11:45:08 +0800

    Update fetchtest.js

--- fetchtest.js
+++ fetchtest.js
@@ -5,6 +5,6 @@
 
 //fetch('https://example.com/')
 
-fetch('https://example.com/',{method:get,mode:'cors'})
+fetch('https://example.com/',{method:'get',mode:'cors'})
 .then(res=>{console.log(res);return res.text();})
 .then(txt=>console.log(txt))
